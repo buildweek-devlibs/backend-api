caf9a06fba7fda2cccc0249542ee258cd231276d
netrunnerd <41193007+netrunnerd@users.noreply.github.com>
2019-09-11 03:17:18 -0400

    add first iteration of users controller/router

---
+++ controllers/users.js
@@ -0,0 +1,157 @@
+const users = require('../models/users');
+
+exports.one = async (req, res) => {
+  const { id } = req.params;
+  
+  try {
+    const user = await users.findById(id);
+
+    if (user) {
+      res.json({
+        success: true,
+        user
+      });
+    } else {
+      res.status(404).json({
+        message: "User does not exist.",
+        success: false
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      error,
+      message: "User could not be retrieved.",
+      success: false
+    });
+  }
+};
+
+exports.libs = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const libs = await users.libs(id);
+
+    if (libs.length > 0) {
+      res.json({
+        libs,
+        success: true
+      });
+    } else {
+      res.status(404).json({
+        message: "No libs for this user.",
+        success: false
+      })
+    }
+  } catch (error) {
+    res.status(500).json({
+      error,
+      message: "User's libs could not be retrieved.",
+      success: false
+    });
+  }
+};
+
+exports.new = async (req, res) => {
+  let user = req.body;
+  const { email, user_id } = user;
+  const length = Object.keys(user).length;
+
+  if (!length) {
+    res.status(400).json({
+      message: "No info.",
+      success: false
+    });
+  } else if (!email || !user_id) {
+    res.status(400).json({
+      message: "No email or user_id.",
+      success: false
+    });
+  } else {
+    try {
+      [ user ] = await users.new(user);
+
+      if (user) {
+        res.json({
+          message: "User saved successfully.",
+          success: true
+        });
+      }
+    } catch (error) {
+      res.status(500).json({
+        error,
+        message: "User could not be saved.",
+        success: false
+      });
+    }
+  }
+};
+
+exports.rm = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const user = await users.findById(id);
+
+    if (user) {
+      const removed = await users.rm(id);
+
+      if (removed) {
+        res.json({
+          message: "User deleted.",
+          success: true
+        });
+      }
+    } else {
+      res.status(404).json({
+        message: "User does not exist.",
+        success: false
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      error,
+      message: "User could not be removed.",
+      success: false
+    });
+  }
+};
+
+exports.update = async (req, res) => {
+  const { id } = req.params;
+  const updates = req.body;
+  const { approved } = updates;
+
+  try {
+    let user = await users.findById(id);
+
+    if (user) {
+      if (!user.mod && approved && approved !== user.approved) {
+        res.status(400).json({
+          message: "User can not update approval.",
+          success: false
+        });
+      } else {
+        user = await users.update(id, updates);
+
+        if (user) {
+          res.json({
+            success: true,
+            user
+          });
+        }
+      }
+    } else {
+      res.status(404).json({
+        message: "User does not exist.",
+        success: false
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      error,
+      message: "User could not be modified.",
+      success: false
+    });
+  }
+};
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,10 @@
 const router = require('express').Router();
+const users = require('../controllers/users');
 
-// user routes here...
+router.delete('/:id', users.rm);
+router.get('/:id', users.one);
+router.get('/:id/libs', users.libs);
+router.post('/', users.new);
+router.put('/:id', users.update);
 
 module.exports = router;
