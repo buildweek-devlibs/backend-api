4e05b926dba2d7c25b0730a25ee4ab1059bb17f7
netrunnerd <41193007+netrunnerd@users.noreply.github.com>
2019-09-10 21:50:36 -0400

    move controllers to models because that's what they are... *cough*

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
 const router = require('express').Router();
 
+const usersRouter = require('./users');
+
 router.get('/', (req, res) => {
   res.send(`
     <h1>Looking for API Information?</h1>
     <p>Please see the <a href='https://github.com/buildweek-devlibs/backend-api'>README</a> for more information on where to go!</p>
   `);
 });
+
+router.use('/users', usersRouter);
 
 module.exports = router;
